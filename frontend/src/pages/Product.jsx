338a8f3c1b7a1c9a2a4f4a11868128f8cc5dd565
Puneeth K <kanike.puneeth@gmail.com>
2024-09-26 21:24:31 +0530

    completed the number in the cart icon

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -6,10 +6,11 @@ import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
+
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -80,7 +81,7 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 hover:bg-gray-800 hover:shadow-lg hover:scale-105 transition-all duration-300 ease-in-out transform'>
+          <button onClick={()=>addToCart(productData._id, size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 hover:bg-gray-800 hover:shadow-lg hover:scale-105 transition-all duration-300 ease-in-out transform'>
             ADD TO CART
           </button>
           <hr className="mt-8 sm:w-4/5" />
